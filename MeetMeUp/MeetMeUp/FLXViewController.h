48f08fc98ac8bf0a2701a1e4e05d742ece378eb9
wbenwick <wbenwick@gmail.com>
2014-03-24 22:32:09 -0400

    Initial Build
    Functional table view display based on API key

---
+++ MeetMeUp/MeetMeUp/FLXViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLXViewController.h
+//  MeetMeUp
+//
+//  Created by Administrator on 3/24/14.
+//  Copyright (c) 2014 FileLogix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLXViewController : UIViewController
+
+@end
---
+++ MeetMeUp/MeetMeUp/FLXViewController.m
@@ -0,0 +1,67 @@
+//
+//  FLXViewController.m
+//  MeetMeUp
+//
+//  Created by Administrator on 3/24/14.
+//  Copyright (c) 2014 FileLogix. All rights reserved.
+//
+
+#import "FLXViewController.h"
+
+@interface FLXViewController () <UITableViewDelegate, UITableViewDataSource>
+{
+    NSArray* myEvents;
+    __weak IBOutlet UISearchBar *mySearchBar;
+    __weak IBOutlet UITableView *myTableView;
+}
+@end
+
+@implementation FLXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    
+    NSURL* url = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=32256&text=mobile&time=,1w&key=7b6d30444c5c61626c7c27643d146455"];
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        NSError* error;
+        NSDictionary* myResults;
+        myResults = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+        myEvents = myResults[@"results"];
+        [myTableView reloadData];
+    }];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    return [myEvents count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyEventReusableID"];
+    NSDictionary *myEvent = myEvents[indexPath.row];
+    NSURL* url = [NSURL URLWithString:myEvent[@"title"]];
+    
+    
+    cell.textLabel.text = myEvent[@"group"][@"name"];
+    cell.detailTextLabel.text = myEvent[@"venue"][@"name"];
+    cell.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+    
+    return cell;
+    
+}
+
+@end
